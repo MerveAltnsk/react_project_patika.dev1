230226951410a5129f353a8b6b3044cc36c0c9f6
merve <mervealtinisik99@gmail.com>
2024-10-13 17:47:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity ,  } from 'react-native';
+import Card from './components/Card/Card'
+import styles from './components/Card/card_styles'
+function App() {
+  return (
+    <View style={styles.container} >
+     <Card title="ilhami" text="Abi sen söyle , ben başka bir ilde miyim?"/>
+      <Card title="Yılmaz" text="Benim zevklerim sizin nezdinizde makul bir zemine oturmak zorunda değil."/>
+      <Card title="İlkkan" text="Yahşı günde yar yahşı , Yaman günde yetiş gardaş"/>
+
+
+      <Card 
+      title="phoebe" 
+      text="MY EYES! MY EYES!"
+      />
+
+      <Card 
+      title="Merve" 
+      text="ı got it"
+      />
+      
+    </View>
+
+  );
+}
+
+
+export default App;
